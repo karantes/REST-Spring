461de03873499d6c1cde1895050386fc93e56c0c
kaique <kaique@host.docker.internal>
2021-10-18 21:56:46 -0300

    Projeto inicial

---
+++ src/main/java/br/com/dio/RestSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
