45ecce5e0ce0660bf541609248621eef95cdafeb
kaique <kaique@host.docker.internal>
2021-10-26 00:28:35 -0300

    Iniciando testes com @SpringBootTest

--- src/test/java/br/com/dio/ProdutoTest.java
+++ src/test/java/br/com/dio/ProdutoTest.java
@@ -1,15 +1,29 @@
 package br.com.dio;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.dio.entity.Produto;
+import br.com.dio.exception.ProductPriceException;
+import br.com.dio.service.ProdutoService;
+
+@SpringBootTest
 public class ProdutoTest {
 
-	@Test
-	public void testeFalso() {
+	@Autowired
+	private ProdutoService produtoService;
 
-		assertTrue(true);
+	@Test
+	public void vericicaValorNegativoNoProduto() {
+		Produto produto = new Produto();
+
+		produto.setNome("Teste");
+		produto.setPreco(-10.0);
+
+		assertThrows(ProductPriceException.class, () -> produtoService.save(produto));
 
 	}
 
